e77fe15e190e6d3d6412ef9280bf92f67104db73
Savara Santha Kumar <savarasanthakumar20@gmail.com>
2024-09-10 18:53:57 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false, // true for port 465, false for other ports
+  auth: {
+    // user: "maddison53@ethereal.email",
+    user: "roxane.reichert@ethereal.email",
+    // pass: "jn7jnAPss4f63QBp6D",
+    pass: "AW2SfQbg4GMykkKRTK",
+  },
+});
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: 'roxane.reichert@ethereal.email', // sender address
+    to: "savarasanthakumar20@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+}
+
+main().catch(console.error);
